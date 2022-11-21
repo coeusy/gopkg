9926e613aa59a721e1b6874aa59008cbf363f221
coeusy <xuwenliang.zy@bytedance.com>
2022-11-22 00:20:09 +0800

    add pprof

--- pprof/server.go
+++ pprof/server.go
@@ -1,15 +1,23 @@
 package pprof
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 	_ "net/http/pprof"
 
 	"go.uber.org/zap"
 )
 
-func init() {
+var port int
+
+func InitCMD() {
+	flag.IntVar(&port, "pprof.port", 8080, "int: port for pprof")
+}
+
+func InitPprof() {
 	go func() {
-		err := http.ListenAndServe(":8080", nil)
+		err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 		if err != nil {
 			zap.S().Errorf("start pprof server failed: %s", err.Error())
 		}
---
+++ pprof/server_test.go
@@ -0,0 +1,11 @@
+package pprof
+
+import "testing"
+
+func TestInitCMD(t *testing.T) {
+	InitCMD()
+}
+
+func TestInitPprof(t *testing.T) {
+	InitPprof()
+}
