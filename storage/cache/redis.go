0e28adc4e27dc3cbd6004844762ffc5259017281
coeusy <xuwenliang.zy@bytedance.com>
2022-12-07 16:40:27 +0800

    add redis

--- storage/cache/redis.go
+++ storage/cache/redis.go
@@ -8,12 +8,17 @@ import (
 	"github.com/coeusy/gopkg/cfg"
 )
 
-func NewRedisClientFromConf(conf cfg.RedisConf) *redis.Client {
+func NewRedisClientFromConf(conf cfg.RedisConf) (*redis.Client, error) {
 	opt := &redis.Options{
 		Addr:       fmt.Sprintf("%s:%d", conf.Host, conf.Port),
 		Password:   conf.Password,
 		DB:         conf.DB,
 		MaxRetries: 2,
 	}
-	return redis.NewClient(opt)
+	cli := redis.NewClient(opt)
+	_, err := cli.Ping().Result()
+	if err != nil {
+		return nil, err
+	}
+	return cli, nil
 }
