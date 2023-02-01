46cb1ca6d740f1a320b9af0043df0ad6654d4622
coeusy <xuwenliang.zy@bytedance.com>
2023-02-02 00:57:25 +0800

    convert int8 slice to byte slice

--- conv/bytes.go
+++ conv/bytes.go
@@ -9,6 +9,16 @@ func BytesToString(bytes []byte) string {
 	return *(*string)(unsafe.Pointer(&bytes))
 }
 
+func Int8sToBytes(int8s []int8) []byte {
+	s := (*reflect.SliceHeader)(unsafe.Pointer(&int8s))
+	b := reflect.SliceHeader{
+		Data: s.Data,
+		Len:  s.Len,
+		Cap:  s.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&b))
+}
+
 func StringToBytes(str string) []byte {
 	s := (*reflect.StringHeader)(unsafe.Pointer(&str))
 	b := reflect.SliceHeader{
--- conv/bytes_test.go
+++ conv/bytes_test.go
@@ -12,3 +12,7 @@ func TestStringToBytes(t *testing.T) {
 func TestBytesToString(t *testing.T) {
 	fmt.Println(BytesToString([]byte{1, 2, 3, 4, 5}))
 }
+
+func TestInt8sToBytes(t *testing.T) {
+	fmt.Println(Int8sToBytes([]int8{1, 2, 3, 4, 5}))
+}
